05436927d93ed7241237e433d871e7fde265ae17
edonyM <edonyzpc@gmail.com>
2015-07-18 11:37:14 +0800

    do test on ocr

--- train.py
+++ train.py
@@ -14,9 +14,18 @@ model.train(samples,responses)
 im = cv2.imread('pi.png')
 out = np.zeros(im.shape,np.uint8)
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+cv2.imshow('in', gray)
+key = cv2.waitKey(0)
+thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+#thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+#ret, thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+#cv2.imshow('in', thresh)
+#key = cv2.waitKey(0)
 
 contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(im,contours,-1,(0,0,255),3)
+cv2.imshow('in', thresh)
+key = cv2.waitKey(0)
 
 for cnt in contours:
     if cv2.contourArea(cnt)>50:
